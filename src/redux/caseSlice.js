abf7de1ad16c95959892a76ba6ef28cde51b5a6d
vidakpop <daviskpop@gmail.com>
2025-02-20 11:46:41 +0300

    add case slice with initial state for managing cases

--- src/redux/caseSlice.js
+++ src/redux/caseSlice.js
@@ -3,3 +3,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 import api from '../services/api'
 
+const caseSlice = createSlice({
+    name:'cases',
+    initialState:{cases:[], loading:false},
+})
