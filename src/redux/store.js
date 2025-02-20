bd1195c5fd53908ae36542df2f6e748c2a233903
vidakpop <daviskpop@gmail.com>
2025-02-20 11:20:30 +0300

    add Redux store configuration with user, case, and notification reducers

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import {configureStore} from '@reduxjs/toolkit'
+import userReducer from './userSlice'
+import caseReducer from './caseSlice'
+import notificationReducer from './notificationSlice'
+
+export const store = configureStore({
+    reducer: {
+        user: userReducer,
+        case: caseReducer,
+        notification: notificationReducer
+    }   
+})  
+
+export default store
