6755de67c056fedd8c1ffe8a8eecf718456dbd4a
vidakpop <daviskpop@gmail.com>
2025-02-20 16:39:35 +0300

    fix Navbar component definition typo and add basic structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,9 @@
 import {Link} from 'react-router-dom'
 
 cosnt Navbar = () => {
-    
+    return (
+        <nav className='bg-blue-600 text-white p-4 flex justify-between'>
+
+        </nav>
+    )
 }
