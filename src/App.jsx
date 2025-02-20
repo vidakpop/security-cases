c68d2d38f5074be3120ba821505b82bf5c31c201
vidakpop <daviskpop@gmail.com>
2025-02-20 20:12:59 +0300

    add routing for Cases component in App to enhance navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Sidebar from "./components/Sidebar"
 import Home from "./pages/Home"
+import Cases from "./pages/Cases"
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <div className="flex-grow p-4 md:ml-64">
           <Routes>
             <Route path="/" element={<Home />}/>
+            <Route path="/cases" element={<Cases />}/>
           </Routes>
 
         </div>
