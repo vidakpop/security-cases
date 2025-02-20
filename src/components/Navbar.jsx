a76f47eac3b0dc10d533f488fd85bebeb40baebe
vidakpop <daviskpop@gmail.com>
2025-02-20 19:12:23 +0300

    update project name to 'security', modify Navbar component, and adjust package dependencies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <nav className='bg-blue-600 text-white p-4 flex justify-between'>
             <h1 className='text-xl font-bold'>Case Management</h1>
-            <Link to='/profile'>Profile</Link>
+           {/* <Link to='/profile'>Profile</Link>*/} 
 
         </nav>
     )
