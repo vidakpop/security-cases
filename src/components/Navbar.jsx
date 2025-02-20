15c993ebc5b8c4a144e6197dde530acebc31faf4
vidakpop <daviskpop@gmail.com>
2025-02-20 16:41:44 +0300

    add Profile link to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ cosnt Navbar = () => {
     return (
         <nav className='bg-blue-600 text-white p-4 flex justify-between'>
             <h1 className='text-xl font-bold'>Case Management</h1>
+            <Link to='/profile'>Profile</Link>
 
         </nav>
     )
