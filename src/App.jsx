6ee2c88628d4ee6c5259d1cd063aa94ad4f4dc9f
vidakpop <daviskpop@gmail.com>
2025-02-20 20:05:30 +0300

    add Home component import to App for routing integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Sidebar from "./components/Sidebar"
+import Home from "./pages/Home"
 
 
 const App = () => {
