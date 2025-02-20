3ae4ff7dafddf30a652ec531425a7292cad779fc
vidakpop <daviskpop@gmail.com>
2025-02-20 19:52:55 +0300

    add Sidebar component to App for enhanced navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import Navbar from "./components/Navbar"
+import Sidebar from "./components/Sidebar"
 
 
 const App = () => {
    return (
     <Router>
       <Navbar/>
+      <div className="flex">
+        <Sidebar/>
+
+      </div>
     </Router>
    )
 }
