5ebdbc6fc5e49175c23646baf6354b2618b87b7a
vidakpop <daviskpop@gmail.com>
2025-02-20 17:07:58 +0300

    fix Navbar component syntax error and update styling with Tailwind CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import {Link} from 'react-router-dom'
 
-cosnt Navbar = () => {
+const Navbar = () => {
     return (
         <nav className='bg-blue-600 text-white p-4 flex justify-between'>
             <h1 className='text-xl font-bold'>Case Management</h1>
