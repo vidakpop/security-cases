68a263ad0e24f952aebb6cf5f92d0c5162f94071
vidakpop <daviskpop@gmail.com>
2025-02-20 11:17:34 +0300

    configure Redux store with user, case, and notification reducers

--- src/services/store.js
+++ src/services/store.js
@@ -1,5 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
-import useReducer from './userSlice'
+import userReducer from './userSlice'
 import caseReducer from './caseSlice'
 import notificationReducer from './notificationSlice'
 
+export const store = configureStore({
+    reducer: {
+        user: userReducer,
+        case: caseReducer,
+        notification: notificationReducer
+    }   
+})  
