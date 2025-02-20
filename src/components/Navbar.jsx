84f55dd989a5afccab13fbf5c96b4e7e56e398b4
vidakpop <daviskpop@gmail.com>
2025-02-20 16:40:34 +0300

    add title to Navbar component for better branding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 cosnt Navbar = () => {
     return (
         <nav className='bg-blue-600 text-white p-4 flex justify-between'>
+            <h1 className='text-xl font-bold'>Case Management</h1>
 
         </nav>
     )
