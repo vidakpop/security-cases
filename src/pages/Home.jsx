ee43aa059b0528f54026fb2bc97eded90ec00617
vidakpop <daviskpop@gmail.com>
2025-02-20 19:59:22 +0300

    enhance Home component with fade-in animation and improved styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,12 @@ import { div } from 'motion/react-client'
 
 const Home = () => {
     return (
-        <motion.div>
+        <motion.div
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{duration:1}}
+            className='flex flex-col items-center justify-center h-screen bg-gray-950 text-white p-6'
+        >
 
         </motion.div>
     )
