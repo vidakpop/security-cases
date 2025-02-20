1e14ccb4a60a500de407e1e27a2c1d0115925448
vidakpop <daviskpop@gmail.com>
2025-02-20 19:34:29 +0300

    add animation effects to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,8 @@ const Navbar = () => {
     return (
         <motion.nav 
             initial={{opacity:0,y:-20}}
+            animate={{opacity:1,y:0}}
+            transition={{duration:0.5}}
             className='bg-blue-600 text-white p-4 flex justify-between'>
             <h1 className='text-xl font-bold'>Case Management</h1>
            {/* <Link to='/profile'>Profile</Link>*/} 
