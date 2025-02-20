4447341c10c3913c6a451c4f4c9fa306f37fd835
vidakpop <daviskpop@gmail.com>
2025-02-20 12:16:55 +0300

    add addNotification reducer to notification slice for managing notifications

--- src/redux/notificationSlice.js
+++ src/redux/notificationSlice.js
@@ -2,3 +2,13 @@
 
 import {createSlice} from '@reduxjs/toolkit'
 
+const notificationSlice = createSlice({
+    name: 'notification',
+    initialState: {messages: []},
+    reducers: {
+        addNotification: (state, action) => {
+            state.messages.push(action.payload)
+        },
+        
+    }
+})
